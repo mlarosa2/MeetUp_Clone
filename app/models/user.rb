96affd73208e2185edccd8bf417d81f24c03e124
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-02 12:00:56 -0400

    add BCrypt and User model methods for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,39 @@
 class User < ActiveRecord::Base
   validates :email, :username, :session_token, :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :generate_session_token
+  attr_reader :password
 
-  after_initialize
+  def self.create_session_token
+    SecureRandom::urlencode_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.create_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def generate_session_token
+    self.session_token = User.create_session_token
+  end
 end
