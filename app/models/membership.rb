499572884ba89347f10635ae1fdfac873a4dd256
App Academy Student <appacademy@Apps-Mac-mini-89.local>
2016-08-03 18:29:44 -0400

    correct through associations

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,18 @@
 class Membership < ActiveRecord::Base
   validates :group_id, :member_id, presence: true
   validates :group_id, uniqueness: { scope: :member_id }
+
+  belongs_to(
+    :group,
+    class_name: "Group",
+    foreign_key: :group_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :member,
+    class_name: "User",
+    foreign_key: :member_id,
+    primary_key: :id
+  )
 end
