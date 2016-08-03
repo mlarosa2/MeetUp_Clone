3ce7868904c9bd6b52e3c88c2e7cbf787be46051
App Academy Student <appacademy@Apps-Mac-mini-89.local>
2016-08-03 13:57:11 -0400

    add validations and moderator relation to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,10 @@
 class Group < ActiveRecord::Base
+  validates :title, :moderator_id, :description, :city, :state, presence: true
+
+  has_one(
+    :moderator,
+    class_name: "User",
+    foreign_key: :moderator_id,
+    primary_key: :id
+  )
 end
