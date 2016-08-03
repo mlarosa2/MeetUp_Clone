cf4441b6104055c18f1bfdf35a3ca29e961c2fdb
App Academy Student <appacademy@Apps-Mac-mini-89.local>
2016-08-03 14:08:28 -0400

    create groups controller and views

---
+++ app/controllers/api/groups_controller.rb
@@ -0,0 +1,47 @@
+class Api::GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+    render @groups
+  end
+
+  def show
+    @group = Group.find(params[:id])
+
+    render @group
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      render @group
+    else
+      render json: @group.errors.full_messages, status: 412
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      render @group
+    else
+      render json: @group.errors.full_messages, status: 412
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    title = @group.title
+    if @group.destroy
+      render json: "#{title} deleted"
+    else
+      render json: @group.errors.full_messages, status: 412
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :moderator_id, :description, :city, :state)
+  end
+end
