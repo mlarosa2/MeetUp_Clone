72ab6e2aed58b4cbee486383be855eb46e12f0cf
App Academy Student <appacademy@Apps-Mac-mini-89.local>
2016-08-03 13:43:26 -0400

    create routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show, :update]
+    resources :groups, only: [:create, :show, :update, :index, :destroy]
     resource :session, only: [:create, :destroy]
   end
 
