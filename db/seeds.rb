00bfab3e8ffd6716038cabf8c9a5a01212a2f815
App Academy Student <appacademy@Apps-Mac-mini-89.local>
2016-08-03 16:39:33 -0400

    add seeds for users, groups, and memberships

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 users = User.create([
-    { username: "pikachu", email: "pik@chu.com", password: "whatever" },
-    { username: "charmander", email: "char@mander.com", password: "whatever" },
-    { username: "porygon", email: "pory@gon.com", password: "whatever" },
-    { username: "abra", email: "ab@ra.com", password: "whatever" },
-    { username: "magikarp", email: "magi@karp.com", password: "whatever" }
-  ])
+  { username: "pikachu", email: "pik@chu.com", password: "whatever" },
+  { username: "charmander", email: "char@mander.com", password: "whatever" },
+  { username: "porygon", email: "pory@gon.com", password: "whatever" },
+  { username: "abra", email: "ab@ra.com", password: "whatever" },
+  { username: "magikarp", email: "magi@karp.com", password: "whatever" }
+])
+
+groups = Group.create([
+  { title: "Shock Therapy", moderator_id: 1, description: "Get shocked by this therapy", city: "Marlton", state: "NJ" },
+  { title: "ESP Enthusiasts", moderator_id: 4, description: "Hone your ESP skills", city: "Philadelphia", state: "PA" },
+  { title: "Splash", moderator_id: 5, description: "Splash", city: "New York", state: "NY"}
+])
+
+memberships = Membership.create([
+  { group_id: 1, member_id: 1 },
+  { group_id: 1, member_id: 2 },
+  { group_id: 1, member_id: 3 },
+  { group_id: 1, member_id: 4 },
+  { group_id: 1, member_id: 5 },
+  { group_id: 2, member_id: 3 },
+  { group_id: 2, member_id: 4 },
+  { group_id: 2, member_id: 1 },
+  { group_id: 3, member_id: 5 },
+  { group_id: 3, member_id: 2 }
+])
