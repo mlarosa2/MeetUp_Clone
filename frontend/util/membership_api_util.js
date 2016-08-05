c341faf6c656001c4b20fc0844555f2802ef5d89
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-05 09:55:28 -0400

    create method fetchAllMemberships

--- frontend/util/membership_api_util.js
+++ frontend/util/membership_api_util.js
@@ -1,29 +1,35 @@
 const MembershipApiUtil = {
   joinGroup(data, successCallback) {
     $.ajax({
-      url: "/api/memberships",
-      method: "POST",
-      data: data,
+      url    : "/api/memberships",
+      method : "POST",
+      data   : data,
       success(dat) {
         successCallback(dat);
       },
-      error(dat) {
-        return dat;
-      }
+      error(dat) { return dat; }
     });
   },
   leaveGroup(data, successCallback) {
     $.ajax({
-      url: `api/memberships/${data.membership.id}`,
-      method: "DELETE",
+      url    : `api/memberships/${data.membership.id}`,
+      method : "DELETE",
       success(dat) {
         successCallback(dat);
       },
-      error(dat) {
-        return dat;
-      }
+      error(dat) { return dat; }
     });
   },
+  fetchAllMemberships(groupId, successCallback) {
+    $.ajax({
+      url    : `api/memberships?group_id=${(groupId)}`,
+      method : "GET",
+      success(dat) {
+        successCallback(dat);
+      },
+      error(err) { return err; }
+    });
+  }
 };
 
 module.exports = MembershipApiUtil;
