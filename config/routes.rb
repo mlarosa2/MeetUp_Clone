d5b7770cd10bcee8ca042ed4f733bf63ecb73584
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-08 15:30:20 -0400

    add update to routes for Events

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show, :update]
     resources :groups, only: [:create, :show, :update, :index, :destroy]
     resources :memberships, only: [:create, :destroy, :index, :show]
+    resources :events, only: [:create, :destroy, :index, :show, :update]
     resource :session, only: [:create, :destroy]
   end
 
