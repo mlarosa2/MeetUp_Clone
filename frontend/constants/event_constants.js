502c773d1e9a3d430a08950eba098ee2e2a3b9e8
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-08 17:44:43 -0400

    remove unnecessary EDIT_EVENT constant

--- frontend/constants/event_constants.js
+++ frontend/constants/event_constants.js
@@ -2,8 +2,7 @@ EventConstants = {
   RECEIVE_EVENTS : "RECEIVE_EVENTS",
   RECEIVE_EVENT  : "REVEIVE_EVENT",
   CREATE_EVENT   : "CREATE_EVENT",
-  EDIT_EVENT     : "EDIT_EVENT",
-  DELETE_EVENT   : "DELETE_EVENT" 
+  DELETE_EVENT   : "DELETE_EVENT"
 };
 
 module.exports = EventConstants;
