1c5c8c169491b48858dd13cfb3172bf964d1ea57
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-09 15:37:53 -0400

    Create through relationships for users and events via rsvps

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,14 @@ class Event < ActiveRecord::Base
   validates :group_id, :title, :description, :start_time, :end_time, presence: true
   validate :date_is_in_future?, :end_time_comes_after?
 
+  has_many(
+    :attendees,
+    through: :rsvps,
+    source: :user
+  )
+
   def date_is_in_future?
-    
+
   end
 
   def end_time_comes_after?
