62f23119bcaa7b0488d7db8d38773d0c938c0ff9
App Academy Student <appacademy@Apps-Mac-mini-25.local>
2016-08-09 10:23:52 -0400

    add admin prop to EventIndex

--- frontend/components/group_description.jsx
+++ frontend/components/group_description.jsx
@@ -22,7 +22,7 @@ const GroupDescription = React.createClass({
     return(
       <div>
         <div>{this.state.group.group.description}</div>
-        <EventIndex groupId={this.props.params.groupId} />
+        <EventIndex groupId={this.props.params.groupId} admin={this.props.params.moderator_id} />
       </div>
     );
   }
